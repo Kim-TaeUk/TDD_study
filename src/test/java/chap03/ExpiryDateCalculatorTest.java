9ecce580919bd1b018d77e31b3bc6d70c1dd657e
Kim-TaeUk <xodnr0130@naver.com>
2023-10-23 05:41:37 +0900

    첫 납부일 일자와 만료일 납부일 일자가 같지 않을 때 만료일 계산 테스트 -> 테스트 코드 추가

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -51,6 +51,17 @@ public class ExpiryDateCalculatorTest {
         );
     }
 
+    @Test
+    void 첫_납부일과_만료일_일자가_다를때_만원_납부하면_첫_납부일_기준으로_다음_만료일_정함() {
+        PayData payData = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 1, 31))
+                .billingDate(LocalDate.of(2019, 2, 28))
+                .payAmount(10_000)
+                .build();
+
+        assertExpiryDate(payData, LocalDate.of(2019, 3, 31));
+    }
+
     private void assertExpiryDate(
             PayData payData,
             LocalDate expectedExpiryDate
