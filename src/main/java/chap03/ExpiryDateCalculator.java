8c396c2314a6c0c1b6a7821a2f65b776958f2825
Kim-TaeUk <xodnr0130@naver.com>
2023-10-24 03:07:47 +0900

    메서드로 분리 : 일이 같은지(isSameDayOfMonth), 주어진 월의 마지막 일자 반환(lastDayOfMonth)

--- src/main/java/chap03/ExpiryDateCalculator.java
+++ src/main/java/chap03/ExpiryDateCalculator.java
@@ -16,10 +16,11 @@ public class ExpiryDateCalculator {
 
     private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
         LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
 
-        if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
-            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+        if (!isSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+
             if (dayLenOfCandiMon < dayOfFirstBilling) {
                 return candidateExp.withDayOfMonth(dayLenOfCandiMon);
             }
@@ -27,5 +28,13 @@ public class ExpiryDateCalculator {
         } else {
             return candidateExp;
         }
+    }
+
+    private boolean isSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1.getDayOfMonth() == date2.getDayOfMonth();
+    }
+
+    private int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
     }
 }
