54c597493815d1a6e83a20bc1207bdcc7644ce74
Kim-TaeUk <xodnr0130@naver.com>
2023-11-24 20:03:39 +0900

    AutoDebitRegisterStubTest: 대역 이용하여 테스트 코드 작성

---
+++ src/test/java/chap07/autodebit/AutoDebitRegisterStubTest.java
@@ -0,0 +1,30 @@
+package chap07.autodebit;
+
+import static chap07.autodebit.CardValidity.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AutoDebitRegisterStubTest {
+	private AutoDebitRegister register;
+	private StubCardNumberValidator stubValidator;
+	private JpaAutoDebitInfoRepository stubRepository;
+
+	@BeforeEach
+	void setUp() {
+		stubValidator = new StubCardNumberValidator();
+		stubRepository = new JpaAutoDebitInfoRepository();
+		register = new AutoDebitRegister(stubValidator, stubRepository);
+	}
+
+	@Test
+	void invalidCard() {
+		stubValidator.setInvalidNo("111122223333");
+
+		AutoDebitReq req = new AutoDebitReq("user1", "111122223333");
+		RegisterResult result = register.register(req);
+
+		assertEquals(INVALID, result.getValidity());
+	}
+}
