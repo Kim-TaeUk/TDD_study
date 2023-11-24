c2530dd7f0abc3b933214a18406c4e0dbdb71087
Kim-TaeUk <xodnr0130@naver.com>
2023-11-24 18:45:13 +0900

    RegisterResult : 자동 이체 등록 결과 나타냄

---
+++ src/main/java/chap07/autodebit/RegisterResult.java
@@ -0,0 +1,19 @@
+package chap07.autodebit;
+
+import static chap07.autodebit.CardValidity.*;
+
+public class RegisterResult {
+	private CardValidity validity;
+
+	public RegisterResult(CardValidity validity) {
+		this.validity = validity;
+	}
+
+	public static RegisterResult error(CardValidity validity) {
+		return new RegisterResult(INVALID);
+	}
+
+	public static RegisterResult success() {
+		return new RegisterResult(VALID);
+	}
+}
